edc002b263eaaa8ed240b595e3e9fb3dffe7da54
Hosea <iamsuccess0708@gmail.com>
2026-05-16 08:56:44 +0200

    update

--- grip/main.py
+++ grip/main.py
@@ -164,7 +164,8 @@ def get_effective_decision(trace_id: str, _: None = Depends(require_api_key)):
 
 
 SUPABASE_URL = "https://lxldqhgevpssgkqtosnz.supabase.co"
-SUPABASE_KEY = "YOUR_REAL_KEY_HERE"
+SUPABASE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imx4bGRxaGdldnBzc2drcXRvc256Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3Nzc1MDEwOTYsImV4cCI6MjA5MzA3NzA5Nn0.Gngx4y6VMrOBISyICnA-pcHpp3NIWq_VITKOTskg7MQ"
+"
 
 @app.post("/login")
 def login(payload: dict = Body(...)):
